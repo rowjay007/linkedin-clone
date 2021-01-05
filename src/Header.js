a7cf8a737f74338b841cf9925811d7ac7fcc485d
Rowland Adimoha <rowlives@gmail.com>
2021-01-05 12:44:15 +0100

    add material ui core and icons

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>This is the header</h1>
+
+      <div className="header__left">
+        <img src="" alt="" />
+      </div>
+      <div className="header__right">
+        <input type="text" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
