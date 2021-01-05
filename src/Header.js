bba2c7db5faca309d69b5564f2ef835a4bb72be3
Rowland Adimoha <rowlives@gmail.com>
2021-01-05 13:06:12 +0100

    create header component

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,23 @@
 import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
 
 const Header = () => {
   return (
     <div className="header">
-      <h1>This is the header</h1>
-
       <div className="header__left">
-        <img src="" alt="" />
+        <img
+          src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg"
+          alt=""
+        />
+        <div className="header__search">
+          {" "}
+          <SearchIcon />
+          <input type="text" />
+        </div>
       </div>
-      <div className="header__right">
-        <input type="text" />
-      </div>
+
+      <div className="header__right"></div>
     </div>
   );
 };
