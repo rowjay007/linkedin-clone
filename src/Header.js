41d7b81918c63c50a6f3559cfef187e8365b4e6f
Rowland Adimoha <rowlives@gmail.com>
2021-01-05 14:15:42 +0100

    create headerOption component with material ui icons

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
+import HeaderOption from "./HeaderOption";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import HomeIcon from "@material-ui/icons/Home";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 const Header = () => {
   return (
@@ -17,7 +23,17 @@ const Header = () => {
         </div>
       </div>
 
-      <div className="header__right"></div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption
+          avatar="https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png"
+          title="Rowjay"
+        />
+      </div>
     </div>
   );
 };
