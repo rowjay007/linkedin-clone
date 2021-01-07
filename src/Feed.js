a152d029e32239fc4612a487c4f714373fa542a1
Rowland Adimoha <rowlives@gmail.com>
2021-01-07 12:31:17 +0100

    create post components

--- src/Feed.js
+++ src/Feed.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import CreateIcon from "@material-ui/icons/Create";
-
 import "./Feed.css";
+import InputOption from "./InputOption";
+import ImageIcon from "@material-ui/icons/Image";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
+import Post from "./Post";
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+
+  const sendPost = e => {
+      e.preventDefault();
+  }
   return (
     <div className="feed">
       <div className="feed__inputContainer">
@@ -10,10 +20,29 @@ const Feed = () => {
           <CreateIcon />
           <form>
             <input type="text" />
-            <button type="submit">Send</button>
+            <button onClick={sendPost} type="submit">Send</button>
           </form>
         </div>
+        <div className="feed__inputOptions">
+          <InputOption Icon={ImageIcon} title="Photo" color="#70b5f9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#e7a33e" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#c0cbcd" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="photo"
+            color="#7fc15e"
+          />
+        </div>
       </div>
+      {posts.map((post) => (
+          
+
+      ))}
+      <Post
+        name="Rowland Adimoha"
+        description="This is a test"
+        message="WOW this worked"
+      />
     </div>
   );
 };
