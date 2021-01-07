b4d86863787e2444b4438e08bfff459f7439d165
Rowland Adimoha <rowlives@gmail.com>
2021-01-07 11:29:28 +0100

    create feed component

---
+++ src/Feed.js
@@ -0,0 +1,21 @@
+import React from "react";
+import CreateIcon from "@material-ui/icons/Create";
+
+import "./Feed.css";
+const Feed = () => {
+  return (
+    <div className="feed">
+      <div className="feed__inputContainer">
+        <div className="feed__input">
+          <CreateIcon />
+          <form>
+            <input type="text" />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Feed;
