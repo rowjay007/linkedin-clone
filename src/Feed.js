7ba8a2aaf02ae45bd6294ae91b40f25751e6f01e
Rowland Adimoha <rowlives@gmail.com>
2021-01-11 09:20:35 +0100

    create userSlice component in the app component

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CreateIcon from "@material-ui/icons/Create";
 import "./Feed.css";
 import InputOption from "./InputOption";
@@ -7,20 +7,52 @@ import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
 import EventNoteIcon from "@material-ui/icons/EventNote";
 import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
 import Post from "./Post";
+import firebase from "firebase";
+import { db } from "./firebase";
+
 const Feed = () => {
   const [posts, setPosts] = useState([]);
+  const [input, setInput] = useState("");
 
-  const sendPost = e => {
-      e.preventDefault();
-  }
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        );
+      });
+  }, []);
+
+  const sendPost = (e) => {
+    e.preventDefault();
+    db.collection("posts").add({
+      name: "Rowland Adimoha",
+      description: "this is a test",
+      message: input,
+      photoUrl: "",
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    setInput("");
+  };
   return (
     <div className="feed">
       <div className="feed__inputContainer">
         <div className="feed__input">
           <CreateIcon />
           <form>
-            <input type="text" />
-            <button onClick={sendPost} type="submit">Send</button>
+            <input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              type="text"
+            />
+            <button onClick={sendPost} type="submit">
+              Send
+            </button>
           </form>
         </div>
         <div className="feed__inputOptions">
@@ -34,15 +66,15 @@ const Feed = () => {
           />
         </div>
       </div>
-      {posts.map((post) => (
-          
-
+      {posts.map(({ id, data: { name, description, message, photoUrl } }) => (
+        <Post
+          key={id}
+          name={name}
+          description={description}
+          message={message}
+          photoUrl={photoUrl}
+        />
       ))}
-      <Post
-        name="Rowland Adimoha"
-        description="This is a test"
-        message="WOW this worked"
-      />
     </div>
   );
 };
---
+++ src/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <img
+        src="https://blogs.ohsu.edu/occupational-health-sciences/files/2020/06/linkedinlogo-831x424.jpg"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Login;
